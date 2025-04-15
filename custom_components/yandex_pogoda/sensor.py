e97920ad33dca6f9abcacdb07b06327cd7ed68d3
Pavel Chertkov <pchertkov@gmail.com>
2025-04-15 12:39:45 +0000

    изменен device_class и state_class для сенсора `wind_bearing`

--- custom_components/yandex_pogoda/sensor.py
+++ custom_components/yandex_pogoda/sensor.py
@@ -98,7 +98,7 @@ WEATHER_SENSORS: tuple[SensorEntityDescription, ...] = (
         icon="mdi:compass-rose",
         translation_key=ATTR_API_WIND_BEARING,
         native_unit_of_measurement=DEGREE,
-        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.WIND_DIRECTION,
     ),
     SensorEntityDescription(
         key=ATTR_WIND_INTERCARDINAL_DIRECTION,
